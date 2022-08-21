6bf7bddfcb6091bed1ed5d207291055d38407e75
Saleh Jalili <salehjalili36@gmail.com>
2022-08-21 15:46:50 +0430

    compelete favorite page

--- src/components/dashboard-page/FavoriteCoursesEmpty.js
+++ src/components/dashboard-page/FavoriteCoursesEmpty.js
@@ -1,15 +1,23 @@
-import { Grid, Typography } from "@mui/material";
+import Link from "next/link";
+import { Grid, Typography, Link as MuiLink } from "@mui/material";
 // mui => theme
-import { flexCenter } from "mui/theme/commonStyles";
+import { flexAlignCenter, flexCenter } from "mui/theme/commonStyles";
+// components
+import { ContainedButton } from "shared/Button";
 
 function FavoriteCoursesEmpty() {
   return (
-    <Grid container>
+    <Grid container sx={{ ...flexAlignCenter, height: "80vh" }}>
       <Grid xs={12} sx={{ ...flexCenter, flexDirection: "column" }}>
         <img src="/vectors/heart.png" alt="heart" />
         <Typography fontSize="14px" color="primary.main" mt={2}>
           هنوز هیچ محصولی را به علاقه مندی ها اضافه نکردید
         </Typography>
+        <Link href="/courses">
+          <MuiLink>
+            <ContainedButton width="130px" margin="1rem 0 0 0">دوره ها</ContainedButton>
+          </MuiLink>
+        </Link>
       </Grid>
     </Grid>
   );
